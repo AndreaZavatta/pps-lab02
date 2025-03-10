b6271edfd85ea2dc29ac23ae76abc9182acd4ea6
Andrea Zavatta <zavattaandrea@gmail.com>
2025-03-10 20:24:36 +0100

    create recursive function for power with tail and tested it

--- src/main/scala/u02/Exercise3.scala
+++ src/main/scala/u02/Exercise3.scala
@@ -1,6 +1,18 @@
 package u02
 
+import scala.annotation.tailrec
+
 object Exercise3 extends App :
   def power(base: Double, exponent: Int): Double = exponent match
     case x if x == 0 => 1
     case _ => base * power(base, exponent - 1)
+
+  def powerTail(base: Double, exponent: Int): Double = {
+    @tailrec
+    def helper(base: Double, exponent: Int, acc: Double): Double = exponent match
+      case 0 => acc
+      case _ if exponent > 0 => helper(base, exponent - 1, acc * base)
+      case _ => helper(base, exponent + 1, acc / base)
+
+    helper(base, exponent, 1.0)
+  }
--- src/test/scala/u02/Exercise3Test.scala
+++ src/test/scala/u02/Exercise3Test.scala
@@ -9,3 +9,6 @@ class Exercise3Test:
   val bias = 0.00001
   @Test def testPowerRecursive(): Unit =
     assertEquals(27, power(3,3), bias)
+
+  @Test def testPowerTailRecursive(): Unit =
+    assertEquals(27, powerTail(3, 3), bias)
