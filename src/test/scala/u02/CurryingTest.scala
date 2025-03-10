abfe8d6898f1f45f61b442fbb0b6fe4d1635de97
Andrea Zavatta <zavattaandrea@gmail.com>
2025-03-10 11:07:30 +0100

    made delta variable as a field in the test class

--- src/test/scala/u02/CurryingTest.scala
+++ src/test/scala/u02/CurryingTest.scala
@@ -5,31 +5,26 @@ import org.junit.Assert.*
 import Currying.*
 
 class CurryingTest:
+  val delta = 0.0001
 
   @Test def testDevideWithNoCurrying(): Unit =
-    val delta = 0.0001
     assertEquals(10, divide(20.0,2.0), delta)
 
   @Test def testDevideWithCurrying(): Unit =
-    val delta = 0.0001
     assertEquals(10, curriedDivide(20.0)(2.0), delta)
 
   @Test def testDevideWithCurrying2(): Unit = {
-    val delta = 0.0001
     val curriedDivide20 = curriedDivide(20.0)
     assertEquals(10, curriedDivide20(2.0), delta)
   }
 
   @Test def testMultWithNoCurrying(): Unit =
-    val delta = 0.0001
     assertEquals(40.0, mult(20.0, 2.0), delta)
 
   @Test def testMultWithCurrying(): Unit =
-    val delta = 0.0001
     assertEquals(40.0, curriedMult(20.0)(2.0), delta)
 
   @Test def testMultWithCurrying2(): Unit = {
-    val delta = 0.0001
     val curriedMult20 = curriedMult(20.0)
     assertEquals(40.0, curriedMult20(2.0), delta)
   }
