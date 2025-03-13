0836e0b21a496f552c0d0281db889b634a526447
Andrea Zavatta <zavattaandrea@gmail.com>
2025-03-13 10:17:46 +0100

    add Expr type

--- src/main/scala/u02/Exercise4.scala
+++ src/main/scala/u02/Exercise4.scala
@@ -1,14 +1,17 @@
 package u02
 
+import u02.Exercise4.{Add, Expr, Literal, Multiply}
+
 object Exercise4 extends App :
 
   case class Literal(a: Int)
 
-  case class Add(left: Any, right: Any)
+  case class Add(left: Expr, right: Expr)
 
-  case class Multiply(left: Any, right: Any)
+  case class Multiply(left: Expr, right: Expr)
 
-  def eval(expr: Any): Int = expr match {
+  type Expr = Literal | Add | Multiply
+  def eval(expr: Expr): Int = expr match {
     case Literal(value) => value
     case Add(left, right) => eval(left) + eval(right)
     case Multiply(left, right) => eval(left) * eval(right)
